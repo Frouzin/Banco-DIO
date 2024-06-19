e9be85bea982163aa36cb084416b157e80397d87
Frouzin <matheusereis44@gmail.com>
2024-06-19 14:54:06 -0300

    class conta add

--- Banco.py
+++ Banco.py
@@ -19,11 +19,55 @@ class PessoaFisica(Cliente):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.cpf = cpf
-    
-
 
 class Conta:
+    def __init__(self, numero, cliente):
+        self.saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+        
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero,cliente)
     
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+        
+        if excedeu_saldo:
+            print(" Operação falhou! Você não tem saldo suficiente.")
+
+        elif valor > 0:
+            self._saldo -= valor
+            print("Saque realizado com sucesso!")
+            return True
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+        return False
     
 
 class ContaCorrente(Conta):
